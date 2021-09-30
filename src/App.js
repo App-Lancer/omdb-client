35208370b564acc287be7ccf993a8ef95258e69b
GauthamArunachalam <gauthamarunachalam1998@gmail.com>
2021-09-30 17:15:36 +0530

    fetch data from server

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import {Component} from 'react'
 import './App.css';
 import Card from './components/card/card';
+import { searchMovies } from './controller';
 
 class App extends Component {
 
   state = {
     selectedMovies: [],
+    dataSource : []
   }
 
   render() {
     return (
       <div className="App">
         <div className="nav">
-          <input type="text" className="search" placeholder="Search"/>
+          <input type="text" className="search" placeholder="Search" onChange={searchMovies}/>
           <div className="actions">
             <input type="button" className="saved-btn" value="Saved Comparisons"/>
             {/* <input type="button" className="save-btn" value="Save"/> */}
